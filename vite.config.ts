ae23a139eba09c7308c8f97e2bbe0a329761004d
Falcon.1 <mhamed01023165311@gmail.com>
2026-06-04 20:12:46 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  // السطر ده مهم جداً عشان جيت هاب يقرا المسارات صح جوه رابط موقعك
-  base: '/to-provide-electricity-/',
   plugins: [react()],
-});
+  base: '/to-provide-electricity-/',
+})
