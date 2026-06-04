1db0d2217d38b27f801a45d5a4a21efddd7a201c
Falcon.1 <mhamed01023165311@gmail.com>
2026-06-04 20:28:47 +0300

    Update base path in vite.config.ts
    Change base path in Vite configuration to relative path.

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+// الكود ده بيجبر السيرفر يقرأ من جوه مجلد المستودع بتاعك بالظبط
 export default defineConfig({
   plugins: [react()],
-  base: '/to-provide-electricity-/',
+  base: './', 
 })
