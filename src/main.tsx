9c9f2b4b9b2a00a139eed975cb15f427546f21b8
Falcon.1 <mhamed01023165311@gmail.com>
2026-06-04 19:08:31 +0300

    Fix import statement formatting in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 // src/main.tsx
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom/client'; 
 import App from './App';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
